f305243623f0e9e2e09b9119b427bb4cac41dd59
BriteMagwaza <67102176+BriteMagwaza@users.noreply.github.com>
2020-06-18 15:25:56 -0700

    Add project files.

---
+++ SignInRegistration/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SignInRegistration.Areas.Identity.Data;
+using SignInRegistration.Data;
+
+[assembly: HostingStartup(typeof(SignInRegistration.Areas.Identity.IdentityHostingStartup))]
+namespace SignInRegistration.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<SignInRegistrationDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("SignInRegistrationDbContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options =>
+                {
+                    options.SignIn.RequireConfirmedAccount = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                })
+               
+
+                
+                    .AddEntityFrameworkStores<SignInRegistrationDbContext>();
+            });
+        }
+    }
+}
